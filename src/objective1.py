20769bf61b8bc7da01048625c7029a37027a7c3f
Nathan Brown <scottynate05@yahoo.com>
2020-11-02 19:27:06 -0800

    done

--- src/objective1.py
+++ src/objective1.py
@@ -2,8 +2,24 @@
 
 my_list1 = [1, 2, 3, 4, 5, 6, 7]
 my_list2 = my_list1
+my_list1.append(8)
 
-print(my_list1 is my_list2)
+# print(my_list1 is my_list2)
 
-print(my_list1)
-print(my_list2)
+# print(my_list1)
+# print(my_list2)
+
+# Example Two
+
+my_text1 = 'Lambda School'
+
+my_text2 = my_text1
+
+my_text1 += ' is testing me, and I did not study!'
+
+print(my_text1 is my_text2)
+
+print(my_text1)
+print(my_text2)
+
+# Example Three
